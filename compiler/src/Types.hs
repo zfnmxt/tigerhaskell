937de2f6e8988bc05eff45fb990f223f81b6c6bf
zfnmxt <37446532+zfnmxt@users.noreply.github.com>
2018-06-05 03:57:45 +0200

    Adds Eq instance

--- compiler/src/Types.hs
+++ compiler/src/Types.hs
@@ -8,4 +8,16 @@ data Ty = Int
         | Array Ty
         | Nil
         | Unit
-        deriving (Show, Eq)
+        deriving (Show)
+
+
+instance Eq Ty where
+  Nil == Record _        = True
+  Record _ == Nil        = True
+  Int == Int             = True
+  String == String       = True
+  Array x == Array y     = x == y
+  Nil == Nil             = True
+  Record xs == Record ys = xs == ys
+  Unit == Unit           = True
+  _ == _                 = False
