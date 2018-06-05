e853fe17a00e4e94e05cf533f6e8e77883d22e9b
zfnmxt <37446532+zfnmxt@users.noreply.github.com>
2018-06-05 04:21:48 +0200

    fixes subtype predicate

--- compiler/src/Types.hs
+++ compiler/src/Types.hs
@@ -12,12 +12,13 @@ data Ty = Int
 
 
 (|>) :: Ty -> Ty -> Bool
-Nil |> Record _      = True
-Record xs |> Record ys = and $ zipWith (\(_, tx) (_, ty) -> tx |> ty) xs ys
-Int |> Int           = True
-String |> String     = True
-Array x |> Array y   = x |> y
-Nil |> Nil           = True
-Unit |> Unit         = True
-_ |> _               = False
+Nil |> Record _        = True
+Record xs |> Record ys = length xs == length ys
+                         && and (zipWith (\(idx, tx) (idy, ty) -> (idx == idy) && (tx |> ty)) xs ys)
+Int |> Int             = True
+String |> String       = True
+Array x |> Array y     = x |> y
+Nil |> Nil             = True
+Unit |> Unit           = True
+_ |> _                 = False
 
