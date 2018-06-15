bc190da75f889aef17135fe0e01fcf5d3346d431
zfnmxt <37446532+zfnmxt@users.noreply.github.com>
2018-06-15 18:08:42 +0200

    adds Temp.hs

---
+++ compiler/src/Temp.hs
@@ -0,0 +1,51 @@
+module Temp (
+   Temp (..)
+ , Label
+ , initTemp
+ , mkTemp
+ , mkLabel
+ , initLabel
+) where
+
+import Control.Monad.Trans.State.Lazy (State)
+import qualified Control.Monad.Trans.State.Lazy as S
+
+data Temp     = Outermost | Temp Int
+data Label    = Label {_labelNum :: Int, _labelName :: String}
+type GenTemp  = State Temp
+type GenLabel = State Int
+
+initTemp :: GenTemp ()
+initTemp = S.put Outermost
+
+mkTemp :: GenTemp Temp
+mkTemp = do
+   temp <- S.get
+   case temp of
+     Outermost -> do
+       S.put  $ Temp 1
+       return $ Temp 1
+     Temp x    -> do
+       S.put  $ Temp (x + 1)
+       return $ Temp (x + 1)
+
+initLabel :: GenLabel ()
+initLabel = S.put 0
+
+mkLabel :: GenLabel Label
+mkLabel = do
+  labelNum <- S.get
+  S.put $ labelNum + 1
+  return $ Label {_labelNum = labelNum, _labelName = "L" ++ show labelNum}
+
+mkNamedLabel :: String -> GenLabel Label
+mkNamedLabel name = do
+ label <- mkLabel
+ return $ label {_labelName = name}
+
+
+
+
+
+
+
