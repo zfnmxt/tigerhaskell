f59bdea4a44a1dc1cf986dea0d7189cfec80854f
zfnmxt <37446532+zfnmxt@users.noreply.github.com>
2018-06-16 16:00:39 +0200

    Separates out common Semant/Translate components

---
+++ compiler/src/STEnv.hs
@@ -0,0 +1,174 @@
+{-# LANGUAGE RecordWildCards #-}
+
+module STEnv where
+
+import Data.Map.Lazy (Map)
+import qualified Data.Map.Lazy as M
+import qualified Control.Monad.Trans.State.Lazy as S
+import Control.Monad.State.Lazy (lift)
+import Control.Monad.Trans.State.Lazy (StateT)
+
+import AST
+import Types
+import Temp
+import Frame hiding (Access)
+import qualified Frame as F
+
+type TError  = String
+type STEnvT  = StateT Env (Either TError)
+
+genError :: Show a => a -> String -> STEnvT b
+genError x s = lift . Left $ "Error on term: " ++ show x ++ " with error msg: " ++ s
+
+--------------------------------------------------------------------------------
+-- Level and Access
+--------------------------------------------------------------------------------
+data Level = Outermost | Level { _levelParent  :: Level
+                               , _levelFrame   :: F.Frame
+                               } deriving (Eq, Show)
+
+data Access = Access { _accessLevel   :: Level
+                     , _accessFAccess :: F.Access
+                     }
+              deriving (Eq, Show)
+
+newLevel :: Level -> Label -> [Bool] -> Level
+newLevel parent name args = Level parent frame
+  where frame = F.newFrame name (True:args)
+
+allocLocal :: Level -> Temp -> Bool -> (Level, Access)
+allocLocal Outermost _ _ = error "oops"
+allocLocal level@Level{..} temp esc = (level', Access level' fAccess')
+  where level' = level { _levelFrame = frame'}
+        (frame', fAccess')
+         | esc       = F.allocMem _levelFrame
+         | otherwise = F.allocReg _levelFrame temp
+
+setLevel :: Level -> STEnvT ()
+setLevel level = do
+  env@Env{..} <- S.get
+  S.put env {_envLevel = level}
+
+resParent :: VEnvEntry -> STEnvT ()
+resParent FunEntry{..} = do
+  case _funEntryLevel of
+    Outermost   -> error "oops"
+    Level{..} -> setLevel _levelParent
+
+intLevel :: Level -> Int
+intLevel Outermost = 0
+intLevel Level{..} = intLevel _levelParent + 1
+
+--------------------------------------------------------------------------------
+-- Env Maps
+--------------------------------------------------------------------------------
+data VEnvEntry = VarEntry {_varEntryTy :: Ty, _varEntryAccess  :: Access}
+               | FunEntry { _funEntryArgTys  :: [Ty]
+                          , _funEntryRetTy   :: Ty
+                          , _funEntryLevel   :: Level
+                          , _funEntryLabel   :: Label
+                          }
+               deriving (Show, Eq)
+
+type TExpr = ()
+type TExprTy = (TExpr, Ty)
+type EnvV = Map Id VEnvEntry
+type EnvT = Map TypeId Ty
+data Env  = Env { _envV      :: EnvV
+                , _envT      :: EnvT
+                , _envTemp   :: Int
+                , _envLabel  :: Int
+                , _envLevel  :: Level
+                , _envLevels :: [Level]
+                } deriving (Show, Eq)
+
+--------------------------------------------------------------------------------
+-- Initial env
+--------------------------------------------------------------------------------
+_PREDEFINED_FUNCS = 10
+
+baseTEnv = M.fromList [("int", Int), ("string", String)]
+baseVEnv = M.fromList [ ("print",     FunEntry [String] Unit Outermost (Label 0)            )
+                      , ("flush",     FunEntry [] Unit Outermost (Label 1)                  )
+                      , ("getchar",   FunEntry [] String Outermost (Label 2)                )
+                      , ("ord",       FunEntry [String] Int Outermost (Label 3)             )
+                      , ("chr",       FunEntry [Int] String Outermost (Label 4)             )
+                      , ("size",      FunEntry [String] Int Outermost (Label 5)             )
+                      , ("substring", FunEntry [String, Int, Int] String Outermost (Label 6))
+                      , ("concat",    FunEntry [String, String] String Outermost (Label 7)  )
+                      , ("not",       FunEntry [Int] Int Outermost (Label 8)                )
+                      , ("exit",      FunEntry [Int] Unit Outermost (Label 9)               )
+                      ]
+
+initEnv = Env { _envV       = baseVEnv
+              , _envT       = baseTEnv
+              , _envTemp    = 1
+              , _envLabel   = _PREDEFINED_FUNCS
+              , _envLevels  = [Level Outermost (newFrame (Label 0) [])]
+              , _envLevel   = Level Outermost (newFrame (Label 0) [])
+              }
+
+--------------------------------------------------------------------------------
+-- Env functions
+--------------------------------------------------------------------------------
+
+insertVar :: Bool -> Id -> Ty -> STEnvT ()
+insertVar esc id ty = do
+  env@Env{..} <- S.get
+  temp        <- mkTemp
+  let (level', access) = allocLocal _envLevel temp esc
+  S.put $ env { _envV     = M.insert id (VarEntry ty access) _envV
+              , _envLevel = level'
+              }
+
+insertFun :: [Bool] -> Id -> [Ty] -> Ty -> STEnvT ()
+insertFun escs id argTys resTy = do
+  env@Env{..} <- S.get
+  levelLabel  <- mkLabel
+  funLabel    <- mkLabel
+  let level  = newLevel _envLevel levelLabel escs
+  S.put $ env { _envV      = M.insert id (FunEntry argTys resTy level funLabel) _envV
+              , _envLevel  = level
+              , _envLevels = level:_envLevels
+              }
+
+lookupVar :: Id -> STEnvT VEnvEntry
+lookupVar id = do
+  Env{..} <- S.get
+  case M.lookup id _envV of
+    Just var@VarEntry{..} -> return var
+    Just FunEntry{..}     -> genError id "found function instead of var"
+    Nothing               -> genError id "var not found"
+
+lookupFun :: Id -> STEnvT VEnvEntry
+lookupFun id = do
+  Env{..} <- S.get
+  case M.lookup id _envV of
+    Just VarEntry{..}     -> genError id "found var instead of function"
+    Just fun@FunEntry{..} -> return fun
+    Nothing               -> genError id "var not found"
+
+insertTy :: TypeId -> Ty -> STEnvT ()
+insertTy typeId ty = do
+  env@Env{..} <- S.get
+  S.put $ env { _envT =  M.insert typeId ty _envT }
+
+lookupTy :: TypeId -> STEnvT Ty
+lookupTy tId = do
+  Env{..} <- S.get
+  case M.lookup tId _envT of
+    Just t  -> return t
+    Nothing -> genError tId "type not found"
+
+mkTemp :: STEnvT Temp
+mkTemp = do
+   env@Env{..} <- S.get
+   S.put  $ env { _envTemp = _envTemp + 1}
+   return $ Temp _envTemp
+
+mkLabel :: STEnvT Label
+mkLabel = do
+   env@Env{..} <- S.get
+   S.put  $ env { _envLabel = _envLabel + 1}
+   return $ Label _envLabel
+
