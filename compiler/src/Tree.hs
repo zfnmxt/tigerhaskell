90e1d98cbbbc092f5a9024b55e35aa0b9dc407ce
zfnmxt <37446532+zfnmxt@users.noreply.github.com>
2018-06-16 16:00:51 +0200

    Defines IR

---
+++ compiler/src/Tree.hs
@@ -0,0 +1,31 @@
+module Tree where
+
+import qualified Temp as T
+
+data Exp = Const Int
+         | Name T.Label
+         | TempE T.Temp
+         | BinOp BinOp Exp Exp
+         | Mem Exp
+         | Call Exp [Exp]
+         | ESeq Stm Exp
+
+data Stm = Move Exp Exp
+         | StmExp Exp
+         | Jump Exp [T.Label]
+         | CJump { _cJumpRelOp :: RelOp
+                 , _cJumpExp1  :: Exp
+                 , _cJumpExp2  :: Exp
+                 , _cJumpTrue  :: T.Label
+                 , _cJumpFalse :: T.Label
+                 }
+         | Seq Stm Stm
+         | StmLabel T.Label
+
+data BinOp = Plus | Sub | Mul | Div | And | Or
+             | LShift | RShift | ARShift | XOR
+data RelOp = Equal | NEqual | Lt | Gt | GTE | LTE
+             | ULt | ULTE | UGTE | UGt
+
+
+
