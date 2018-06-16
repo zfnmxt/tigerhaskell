0a4b57338e66bb4f92cbc8f02760b288818b025b
zfnmxt <37446532+zfnmxt@users.noreply.github.com>
2018-06-16 16:41:35 +0200

    Added register option

--- compiler/src/Temp.hs
+++ compiler/src/Temp.hs
@@ -1,38 +1,7 @@
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE RecordWildCards #-}
-
 module Temp where
 
-import Control.Monad.Trans.State.Lazy (StateT, State)
-import Control.Monad.Trans.Class (MonadTrans, lift)
-import Control.Monad.Identity (Identity)
-import qualified Control.Monad.Trans.State.Lazy as S
+import Frame
 
 data Temp      = Temp Int  deriving (Eq, Show)
+               | RTemp Reg
 data Label     = Label Int deriving (Eq, Show)
-type GenTempT  = StateT Temp
-type GenTemp   = GenTempT Identity
-
---nextRTemp :: Monad m => GenTempT m RTemp
---nextRTemp = do
---   temp@Temp{..} <- S.get
---   S.put  $ temp { _tempRTempC = _tempRTempC + 1}
---   return $ RTemp _tempRTempC
---
---nextLabel :: Monad m => GenTempT m Label
---nextLabel = do
---   temp@Temp{..} <- S.get
---   S.put  $ temp { _tempLabelC = _tempLabelC + 1}
---   return $ Label _tempLabelC $ "L" ++ show _tempLabelC
---
---nextNamedLabel :: Monad m => String -> GenTempT m Label
---nextNamedLabel name = do
---   temp@Temp{..} <- S.get
---   S.put  $ temp { _tempLabelC = _tempLabelC + 1}
---   return $ Label _tempLabelC name
---
---class MonadTemp m where
---  mkRTemp       :: m RTemp
---  mkLabel       :: m Label
---  mkNamedLabel  :: String -> m Label
-
