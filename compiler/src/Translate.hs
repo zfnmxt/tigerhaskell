e6142aab1703796a374eb2d84be9027fd4cc5f3d
zfnmxt <37446532+zfnmxt@users.noreply.github.com>
2018-06-16 17:03:22 +0200

    fixes uxCX

--- compiler/src/Translate.hs
+++ compiler/src/Translate.hs
@@ -2,45 +2,55 @@
 
 module Translate where
 
-import Tree hiding (Exp)
-import qualified Tree
-import Frame
+import qualified Tree  as T
+import qualified Frame as F
 import Temp
 import STEnv
 
-data Exp = Ex Tree.Exp
-         | Nx Stm
-         | Cx (Label -> Label -> Stm)
+data Exp = Ex T.Exp
+         | Nx T.Stm
+         | Cx (Label -> Label -> T.Stm)
 
-seqMany :: [Stm] -> Stm
+seqMany :: [T.Stm] -> T.Stm
 seqMany []     = error "oops"
 seqMany (s:ss) = foldr Seq s ss
 
-unEx :: Exp -> STEnvT Tree.Exp
+unEx :: Exp -> STEnvT T.Exp
 unEx (Ex e)      = return $ e
 unEx (Nx s)      = return $ ESeq s (Const 0)
 unEx (Cx genStm) = do
   r <- mkTemp
   t <- mkLabel
   f <- mkLabel
-  let stms = [ Move (TempE r) (Const 1)
+  let stms = [ Move (IReg r) (Const 1)
              , genStm t f
              , StmLabel f
-             , Move (TempE r) (Const 0)
+             , Move (IReg r) (Const 0)
              , StmLabel t
              ]
   return $ ESeq (seqMany stms) (TempE r)
 
-unNX :: Exp -> STEnvT Tree.Stm
+unNX :: Exp -> STEnvT T.Stm
 unNX (Ex e) = return $ StmExp e
 unNX (Nx s) = return s
 unNx (Cx genStm) = do
   e <- unEx (Cx genStm)
   unNX (Ex e)
 
-unCX :: Exp -> STEnvT (Label -> Label -> Stm)
-unCX (Ex e)      = return $ \t f -> StmExp e
-unCX (Nx s)      = return $ \t f -> s
+unCX :: Exp -> STEnvT (Label -> Label -> T.Stm)
+uxCX (Const 0)   = return $ \_ f -> Jump (StmLabel f) [f]
+uxCX (Const 1)   = return $ \t _ -> Jump (StmLabel t) [t]
+unCX (Ex e)      = return $ \t f -> CJump NEqual (Const 0) e t f
+unCX (Nx _)      = error "oops"
 unCX (Cx genStm) = return genStm
+
+simpleVar :: Access -> Level -> STEnvT Exp
+simpleVar Access{..} level =
+  case _accessLoc of
+    InFrame offset ->
+      let memExp = BinOp Plus (RTemp RBP) (Const offset)
+      in return $ Mem memExp
+    InReg reg      ->
+      return IReg reg
 
 
