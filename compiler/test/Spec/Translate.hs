05d9df6a971114aee38ee40dd8b9b26832ee90e5
zfnmxt <37446532+zfnmxt@users.noreply.github.com>
2018-06-22 21:23:22 +0200

    Adds a couple of array/record tests

--- compiler/test/Spec/Translate.hs
+++ compiler/test/Spec/Translate.hs
@@ -2,35 +2,36 @@
 
 module Spec.Translate where
 
-import Test.Hspec
-import Test.QuickCheck
+import           Control.Monad.Trans.State.Lazy (StateT, evalStateT, execStateT,
+                                                 runStateT)
 import qualified Control.Monad.Trans.State.Lazy as S
-import Control.Monad.Trans.State.Lazy (StateT, runStateT, evalStateT, execStateT)
-import qualified Data.Map.Lazy as M
-import Data.Map.Lazy (Map)
+import           Data.Map.Lazy                  (Map)
+import qualified Data.Map.Lazy                  as M
+import           Test.Hspec
+import           Test.QuickCheck
 
-import Spec.AppelTigFiles
-import Spec.Parser (testParseE)
-import Spec.Semant
+import           Spec.AppelTigFiles
+import           Spec.Parser                    (testParseE)
+import           Spec.Semant
 
-import Parser
-import Semant
-import AST
-import Types
-import Translate
-import Frame
-import STEnv
-import qualified Tree as T
-import Tree
-import Temp
-import Registers
+import           AST
+import           Frame
+import           Parser
+import           Registers
+import           Semant
+import           STEnv
+import           Temp
+import           Translate
+import           Tree
+import qualified Tree                           as T
+import           Types
 
 evalStateT' m = evalStateT m initEnv
 execStateT' m = execStateT m initEnv
 
 parseAndTran :: String -> Either TError TExpr
 parseAndTran s = case testParseE s of
-  Left _ -> Left "oops"
+  Left _  -> Left "oops"
   Right x -> evalStateT'(transExpr x)
 
 translateTests :: SpecWith ()
@@ -39,7 +40,7 @@ translateTests = do
   treeTests
 
 stackFrameTests :: SpecWith ()
-stackFrameTests = 
+stackFrameTests =
   describe "Basic stack frame tests" $ do
   it "basic nesting level" $ do
     let fDec = FunDec [FunDef "f" [Field "x" "int" True, Field "y" "string" True] (Just "string") (SExpr "foo")]
@@ -54,31 +55,47 @@ stackFrameTests =
     getVLevel <$> (M.lookup "h"_envV) `shouldBe` Just 2
 
 treeTests :: SpecWith ()
-treeTests = 
+treeTests =
   describe "Basic tree tests" $ do
   it "simple var" $ do
      let vDec    = VarDec $ VarDef "x" (Just "int") (IExpr 5)
      let letExpr = Let [vDec] [VExpr (SimpleVar "x")]
-     let tExpr   = Ex $ Mem $ BinOp T.Plus (IReg (RTemp RBX)) (Const (-4))
+     let tExpr   = Ex $ Mem $ BinOp T.Plus (Const (-4)) (IReg (RTemp RBX)) 
      evalStateT' (transExpr letExpr) `shouldBe` Right (TExpr tExpr Int)
   it "two simple vars" $ do
      let vDec1   = VarDec $ VarDef "x" (Just "int") (IExpr 5)
      let vDec2   = VarDec $ VarDef "y" (Just "int") (IExpr 1)
      let letExpr = Let [vDec1, vDec2] [ExprSeq [VExpr (SimpleVar "x"), VExpr (SimpleVar "y")]]
-     let tExpr   = Ex $ Mem $ BinOp T.Plus (IReg (RTemp RBX)) (Const (-8))
+     let tExpr   = Ex $ Mem $ BinOp T.Plus (Const (-8)) (IReg (RTemp RBX)) 
      evalStateT' (transExpr letExpr) `shouldBe` Right (TExpr tExpr Int)
   it "function call" $ do
      let fDec    = FunDec [FunDef "f" [Field "y" "int" True] (Just "int") (IExpr 1)]
      let letExpr = Let [fDec] [FCall "f" [IExpr 1]]
-     let tExpr   = Ex $ Mem $ BinOp T.Plus (IReg (RTemp RBX)) (Const (-8))
+     let tExpr   = (Ex (Call (Name (Label 11)) [Mem (Const 0),Const 1]))
      evalStateT' (transExpr letExpr) `shouldBe`
-       Right (TExpr (Ex (Call (Name (Label 11)) [Mem (Const 0),Const 1])) Int)
+       Right (TExpr tExpr Int)
   it "nested simple var" $ do
-     let vDec  = VarDec $ VarDef "x" (Just "int") (IExpr 5)
-     let fDec  = FunDec [FunDef "f" [Field "y" "int" True] (Just "int") (VExpr (SimpleVar "x"))]
-     let letExpr = Let [vDec, fDec] [FCall "f" [IExpr 1]]
-     let tExpr   = Ex $ Mem $ BinOp T.Plus (IReg (RTemp RBX)) (Const (-8))
+     let vDec    = VarDec $ VarDef "x" (Just "int") (IExpr 5)
+     let fDec    = FunDec [FunDef "f" [Field "y" "int" True] (Just "int") (VExpr (SimpleVar "y"))]
+     let letExpr = Let [vDec, fDec] [FCall "f" [VExpr (SimpleVar "x")]]
+     let vLoc    = Mem (BinOp T.Plus (Const (-4)) (Mem (BinOp T.Plus (Const 0) (IReg (RTemp RBX)))))
+     let tExpr   = Ex (Call (Name (Label 11)) [Mem (Const 0), vLoc])
      evalStateT' (transExpr letExpr) `shouldBe` Right (TExpr tExpr Int)
-
-
+  it "simple array" $ do
+     let tDec    = TypeDec [Type "arrayT" (ArrayType "int")]
+     let vDec    = VarDec $ VarDef "x" (Just "arrayT") (ArrayExpr "arrayT" (IExpr 5) (IExpr 2))
+     let aExp    = VExpr $ ArrayVar (SimpleVar "x") (IExpr 1)
+     let letExpr = Let [tDec, vDec] [aExp]
+     let tExpr   = Ex (Mem (BinOp Plus (Mem (BinOp Plus (Const (-4)) (IReg (RTemp RBX)))) (BinOp 
+                    Mul (Const 1) (Const 4))))                                                                     
+     evalStateT' (transExpr letExpr) `shouldBe` Right (TExpr tExpr Int)
+  it "simple record" $ do
+     let tDec    = TypeDec [Type "recT" (RecordType ["field1" |: "int", "field2" |: "string"])]
+     let vDec    = VarDec $ VarDef "x" (Just "recT") (RecordExpr "recT"
+                                                        ["field1" |. IExpr 1, "field2" |. SExpr "foo"])
+     let rExp    = VExpr $ FieldVar (SimpleVar "x") "field2"
+     let letExpr = Let [tDec, vDec] [rExp]
+     let tExpr   = Ex (Mem (BinOp Plus (Mem (BinOp Plus (Const (-4)) (IReg (RTemp RBX)))) (BinOp 
+                    Mul (Const 1) (Const 4))))                                                                     
+     evalStateT' (transExpr letExpr) `shouldBe` Right (TExpr tExpr String)
 
